25788db8a847fac865577aa7db5cffce9eccb978
Artem Shkuratov <shuratov2001@mail.ru>
2020-12-14 11:31:30 +0300

    add function to init file that off product

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,5 +1,27 @@
 <?
- for($i = 0; $i < 1000; $i++){
+ $eventManager = \Bitrix\Main\EventManager::getInstance();
+	$eventManager->addEventHandler('iblock', 'OnBeforeIBlockElementUpdate', 'productOFF');
 
-	echo "hello";
- }
+    function productOFF($arFields)
+    {  
+    	global $APPLICATION;
+
+        $select = array("ID", "IBLOCK_ID", "SHOW_COUNTER");
+        $filter = Array("IBLOCK_ID"=>$arFields["IBLOCK_ID"], "ID"=>$arFields["ID"]);
+        
+        $result = CIBlockElement::GetList(
+            array(),
+            $filter,
+            false,
+            false,
+            $select
+        );
+            if($temp = $result->Fetch())
+            {
+                if ( $temp["SHOW_COUNTER"] > 2 && $arFields["ACTIVE"] == "N")
+                {
+                    $APPLICATION->throwException("Товар невозможно деактивировать, у него  ".$temp["SHOW_COUNTER"] . " просмотров ");
+                    return false;
+                }
+            } 
+    }
