2012f072fbf3658c1aff7f66d49d1feb872ede49
Artem Shkuratov <shuratov2001@mail.ru>
2020-12-14 15:47:36 +0300

    add init file

--- ex2/feedback/index.php
+++ ex2/feedback/index.php
@@ -1,4 +1,14 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("feedback");
-?>Text here....<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:main.feedback",
+	"",
+	Array(
+		"EMAIL_TO" => "olegminsk1@mail.ru",
+		"EVENT_MESSAGE_ID" => array("7"),
+		"OK_TEXT" => "Спасибо, ваше сообщение принято.",
+		"REQUIRED_FIELDS" => array("NAME","EMAIL"),
+		"USE_CAPTCHA" => "Y"
+	)
+);?>&nbsp;<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
