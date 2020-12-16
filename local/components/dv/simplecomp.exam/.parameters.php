a79b8c1d1ca076e109e00a5dbde57fd9f77ccc07
Artem Shkuratov <shuratov2001@mail.ru>
2020-12-16 17:23:28 +0300

    added input parameters to visual editor

--- local/components/dv/simplecomp.exam/.parameters.php
+++ local/components/dv/simplecomp.exam/.parameters.php
@@ -1,5 +1,37 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die(); 
 
-	
+	//форматирование входных параметров
+
+	$arComponentParameters = array(
+		 "GROUPS" => array(),
+
+		 "PARAMETERS" => array(
+		 	"CATALOG_IBLOCK_ID" => array(
+			"PARENT" => "BASE",
+			"NAME" => "ID инфоблока с каталогом товаров",
+			"TYPE" => "STRING",
+			"MULTIPLE" => "N",
+			"DEFAULT" => "",
+		 	),
+
+		 	"NEWS_IBLOCK_ID" => array(
+			"PARENT" => "BASE",
+			"NAME" => "ID инфоблока с новостями",
+			"TYPE" => "STRING",
+			"MULTIPLE" => "N",
+			"DEFAULT" => "",
+			),
+
+			"CODE_USER" => array(
+			"PARENT" => "BASE",
+			"NAME" => "Код пользовательского свойства разделов каталога, в котором хранится привязка к новостям",
+			"TYPE" => "STRING",
+			"MULTIPLE" => "N",
+			"DEFAULT" => "",
+			),
+
+			"CACHE_TIME"  =>  array("DEFAULT"=>36000000),
+		),
+	);
 
 ?>
