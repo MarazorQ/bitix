8a03f11e3e81e87eeaa2148cc80b3c348539fd83
Artem Shkuratov <shuratov2001@mail.ru>
2020-12-16 15:59:47 +0300

    complementing the component structure

--- local/components/dv/simplecomp.exam/.description.php
+++ local/components/dv/simplecomp.exam/.description.php
@@ -1,6 +1,10 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die(); 
 
-
+	$arComponentDescription = array(
+    "NAME" => GetMessage("Каталог товаров"),
+    "DESCRIPTION" => GetMessage("Каталог товаров с новостями"),
+    "ICON" => "images/icon.gif",
+    );
 
 
 ?>
