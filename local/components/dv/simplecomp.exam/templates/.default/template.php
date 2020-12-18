2c545542198d260683ffbb4c4512420ccf210add
Artem Shkuratov <shuratov2001@mail.ru>
2020-12-18 14:35:53 +0300

    edit template output

--- local/components/dv/simplecomp.exam/templates/.default/template.php
+++ local/components/dv/simplecomp.exam/templates/.default/template.php
@@ -1,6 +1,30 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+	<div>
+		<h3> Элементов: <?=$arResult['COUNT_ELEMS'];?></h3>
+	    <ul>
+	    <?php foreach ($arResult as $key => $news):?>
 
+	        <?php if ($key == 'COUNT_ELEMS'):?>
+	            <?php continue;?>
+	        <?php endif;?>
 
-  
+	        <?php $headStr = '<b>' . $news['NEWS_NAME'] . '</b> - ' . $news['NEWS_DATE'] . ' (';?>
 
-?>
+	        <?php foreach ($news['DIRECTORY'] as $dirData):?>
+	            <?php $headStr .= ' ' . $dirData['NAME'];?>
+	        <?php endforeach;?>
+
+	        <?php $headStr .= ')';?>
+	        <li> <?=$headStr;?></li>
+	           <ul>
+
+	        <?php foreach ($news['ELEMS'] as $elem):?>
+	            <li> 
+		            <?php echo $elem['NAME'] . ' - ' . $elem['PROPERTY_PRICE_VALUE'] . ' - ' . $elem['PROPERTY_MATERIAL_VALUE'] . ' - ' . $elem['PROPERTY_ARTNUMBER_VALUE'];?>
+	            </li>
+	        <?php endforeach;?>
+	           </ul>
+		<?php endforeach;?>
+	    </ul>
+    </div>
+
